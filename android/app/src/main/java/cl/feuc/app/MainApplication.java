32eb443d082d47d3e27cc1670dca7fcb74c61504
Patricio López <lopezjuripatricio@gmail.com>
2017-02-19 20:09:33 -0300

    Add react-native-linear-gradient

--- android/app/src/main/java/cl/feuc/app/MainApplication.java
+++ android/app/src/main/java/cl/feuc/app/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.horcrux.svg.SvgPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new RNDeviceInfo(),
             new SvgPackage(),
             new ReactNativeConfigPackage(),
