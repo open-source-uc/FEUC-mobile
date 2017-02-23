01aced49c152acc1bd21559b78e10151cbc5f461
Patricio López <lopezjuripatricio@gmail.com>
2017-02-22 22:46:16 -0300

    Add default initiative icon

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,9 @@ export const images = {
   logo: {
     transparent: require('./images/logo-transparent-rect.png'),
   },
+  default: {
+    initiative: require('./images/default-initiative.png'),
+  },
   eventBookmark: require('./images/event-bookmark.png'),
 };
 
