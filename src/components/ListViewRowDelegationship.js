5f7b4a87165decae0299b208202bd9c55fc811a8
Patricio López <lopezjuripatricio@gmail.com>
2017-07-30 21:39:32 -0400

    Remove unused dependency import

--- src/components/ListViewRowDelegationship.js
+++ src/components/ListViewRowDelegationship.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Image, Platform } from "react-native";
 import PropTypes from "prop-types";
-import styled from "styled-components/native";
 import get from "lodash/get";
 import trimStart from "lodash/trimStart";
 
